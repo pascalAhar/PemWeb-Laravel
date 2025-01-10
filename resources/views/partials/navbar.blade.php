19e2643f30056b7a402f78dc516cdcb306beae03
pascalAhar <pascalahar93@ummi.ac.id>
2025-01-10 10:58:00 +0700

    CRUD 1 & 2

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,42 @@
+<div class="sticky-top">
+  <header class="navbar-expand-md">
+    <div class="collapse navbar-collapse" id="navbar-menu">
+      <div class="navbar">
+        <div class="container-xl">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('roles.index') }}">
+                <span class="nav-link-title">Peran</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('users.index') }}">
+                <span class="nav-link-title">Pengguna</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('casts.index') }}">
+                <span class="nav-link-title">Pemeran</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('genres.index') }}">
+                <span class="nav-link-title">Genre</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('movies.index') }}">
+                <span class="nav-link-title">Film</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('reviews.index') }}">
+                <span class="nav-link-title">Ulasan</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</div>
